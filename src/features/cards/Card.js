2d54bc3c9b63611eee6d547094a773a347fdf92b
Kent Haubein <khaubein@gmail.com>
2025-03-04 11:03:40 +0700

    Updated Performance with  shallowEqual to reduce rendering

--- src/features/cards/Card.js
+++ src/features/cards/Card.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
 import { selectCardById } from './cardsSlice';
 
 export default function Card({ id }) {
   // Retrieve the card details for the given cardId from state
-  const card = useSelector(state => selectCardById(state, id));
+  const card = useSelector(state => selectCardById(state, id), shallowEqual);
   const [isFlipped, setIsFlipped] = useState(false);
 
 
-
-return (
+  return (
   <li>
     <button className="card" onClick={() => setIsFlipped(!isFlipped)}>
       {isFlipped ? card.back : card.front}
